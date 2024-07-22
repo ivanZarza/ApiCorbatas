283713a70988747fb5126326299b4ef588cda7be
IvanZarza <ivanzarzaestevez@gmail.com>
2024-07-22 21:06:54 +0200

    wip

--- Api-Crorbatas/src/App.vue
+++ Api-Crorbatas/src/App.vue
@@ -15,7 +15,12 @@ body {
   font-family: Arial, sans-serif;
   margin: 0;
   padding: 0;
-
+  background-image: url('../public/photo_2024-07-22_20-44-11.jpg');
+  background-size: cover;
+  background-repeat:repeat;
+  background-attachment: fixed;
+  background-position: center;
+  background-color: #000;
 }
 
 </style>
